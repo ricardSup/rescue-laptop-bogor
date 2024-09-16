6941ca926ac3b7e7efb959149d6eedf46bf4de7b
richard.s <richardlim97@rocketmail.com>
2024-09-16 18:44:30 +0700

    Updated/Added simply-static-1-1726486545

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-16.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"11e9fae6","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"richardlim123555@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Pengiriman Formulir","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Silakan isi captcha di atas.","confirmationUrl":""}, '.uagb-block-11e9fae6', 16 );
+});
+ });
